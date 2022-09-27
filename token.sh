1dbdbd22e5b25f654962474e710aa57f090f9ecc
Harris2711 <96809941+Harris2711@users.noreply.github.com>
2022-09-27 14:08:07 +0530

    Update token.sh

--- token.sh
+++ token.sh
@@ -1,5 +1,3 @@
 sudo echo $(kubeadm token create --print-join-command) --control-plane --certificate-key $(kubeadm init phase upload-certs --upload-certs | grep -vw -e certificate -e Namespace) > masterjointoken.txt
 
-sudo token=$(cat masterjointoken.txt)
-
-sudo aws secretsmanager update-secret --secret-id secretmaster --secret-string $token
+sudo aws secretsmanager update-secret --secret-id secretmaster --secret-string file://masterjointoken.txt
